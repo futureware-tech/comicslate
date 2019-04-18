57a95056df1bf2ffa1ee4d6fa70cc40c1fcf010e
Artem Sheremet <dot.doom@gmail.com>
2019-04-18 11:59:47 +0200

    Add DatFestComic

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ class MyApp extends StatelessWidget {
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
         primarySwatch: Colors.blue,
+        fontFamily: 'DatFestComic',
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
