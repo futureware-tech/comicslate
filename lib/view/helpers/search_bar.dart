5394c3d42e7cf5f0252199acb32d374894e74d6c
Katarina Sheremet <kate.romanowskaya@gmail.com>
2019-04-23 15:46:54 +0200

    Add search bar widget

---
+++ lib/view/helpers/search_bar.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+typedef SearchCallback = void Function(String input);
+
+class SearchBarWidget extends StatefulWidget implements PreferredSizeWidget {
+  final Widget title;
+  final SearchCallback search;
+
+  const SearchBarWidget({this.title, this.search});
+
+  @override
+  State<StatefulWidget> createState() => SearchBarWidgetState();
+
+  // Flutter documentation for AppBar.preferredSize says:
+  // "the sum of kToolbarHeight and the bottom widget's preferred height".
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
+
+class SearchBarWidgetState extends State<SearchBarWidget> {
+  final TextEditingController _searchController = TextEditingController();
+  bool _isSearchMode = false;
+
+  void _searchTextChanged() {
+    if (_isSearchMode) {
+      widget.search(_searchController.text);
+    } else {
+      widget.search(null);
+    }
+  }
+
+  @override
+  void initState() {
+    _searchController.addListener(_searchTextChanged);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _searchController
+      ..removeListener(_searchTextChanged)
+      ..dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget appBarTitle;
+    Icon actionIcon;
+
+    if (_isSearchMode) {
+      actionIcon = const Icon(Icons.close);
+      appBarTitle = TextField(
+        autofocus: true,
+        controller: _searchController,
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 19,
+        ),
+        decoration: InputDecoration(
+            border: InputBorder.none,
+            hintText: 'Поиск ...',
+            hintStyle: TextStyle(
+              color: Colors.white,
+              fontSize: 19,
+            )),
+      );
+    } else {
+      appBarTitle = widget.title;
+      actionIcon = const Icon(Icons.search);
+    }
+
+    return AppBar(
+      title: appBarTitle,
+      actions: <Widget>[
+        IconButton(
+          icon: actionIcon,
+          onPressed: () {
+            setState(() {
+              if (_isSearchMode) {
+                // TODO(ksheremet): would the user always want to clear it?
+                _searchController.clear();
+                _isSearchMode = false;
+              } else {
+                _isSearchMode = true;
+              }
+            });
+          },
+        )
+      ],
+    );
+  }
+}
